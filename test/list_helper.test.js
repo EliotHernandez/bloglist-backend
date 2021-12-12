55a8bded4f4523146e8aa2f913ba3082c275a1c0
Eliot <eliot@artek.es>
2021-12-12 19:04:20 +0000

    4.4: helper functions and unit tests, step 3

--- test/list_helper.test.js
+++ test/list_helper.test.js
@@ -60,3 +60,120 @@ describe('total likes', () => {
   })
 
 })
+
+describe('favourite blog', () => {
+  const listWithNoBlog = []
+
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  const listWithOneBlogAndNoLikes = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 0,
+      __v: 0
+    }
+  ]
+
+  const listWithThreeBlogs = [
+    {
+      _id: '5a422a851b54a676234d17f7',
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12,
+      __v: 0
+    }
+  ]
+
+  const listWithThreeBlogsAndSameLikes = [
+    {
+      _id: '5a422a851b54a676234d17f7',
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 7,
+      __v: 0
+    }
+  ]
+
+  test('when list has no blogs, return empty object', () => {
+    expect(listHelper.favouriteBlog(listWithNoBlog)).toEqual({})
+  })
+
+  test('when list has only one blog, return that', () => {
+    expect(listHelper.favouriteBlog(listWithOneBlog)).toEqual({
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      likes: 5
+    })
+  })
+
+  test('when list has only one blog and no likes, return that', () => {
+    expect(listHelper.favouriteBlog(listWithOneBlogAndNoLikes)).toEqual({
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      likes: 0
+    })
+  })
+
+  test('when list has several blogs, return the blog with most likes', () => {
+    expect(listHelper.favouriteBlog(listWithThreeBlogs)).toEqual({
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      likes: 12
+    })
+  })
+
+  test('when list has several blogs with same likes, return the first blog', () => {
+    expect(listHelper.favouriteBlog(listWithThreeBlogsAndSameLikes)).toEqual({
+      title: 'React patterns',
+      author: 'Michael Chan',
+      likes: 7
+    })
+  })
+
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -12,7 +12,24 @@ const totalLikes = (blogs) => {
     : blogs.reduce(reducer, 0)
 }
 
+const favouriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return {}
+  }
+  const maxLikes = blogs.reduce((numLikes, blog) => {
+    return blog.likes > numLikes ? blog.likes : numLikes
+  }, 0)
+
+  const maxLikedBlog = blogs.find(blog => blog.likes === maxLikes)
+  return {
+    title: maxLikedBlog.title,
+    author: maxLikedBlog.author,
+    likes: maxLikedBlog.likes
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favouriteBlog
 }
